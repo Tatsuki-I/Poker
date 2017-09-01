ec9856f79a500d9627a5a1a032caf89a2b162b98
Tatsuki <tatsuki@konokaze.jp>
2017-09-01 12:32:27 +0900

    hlint

--- app/Main.hs
+++ app/Main.hs
@@ -9,18 +9,13 @@ import System.Random.Shuffle
 main :: IO ()
 main =  do sls <- shuffleM initCards
            print $ initTexas ps sls
-           print $ getHighestHand $ ((head ((initTexas ps sls) ^. players)) ^. hole) ++ _community (initTexas ps sls)
-           print $ getHighestHand $ ((last ((initTexas ps sls) ^. players)) ^. hole) ++ _community (initTexas ps sls)
+           print $ getHighestHand $ (head (initTexas ps sls ^. players) ^. hole) ++ _community (initTexas ps sls)
+           print $ getHighestHand $ (last (initTexas ps sls ^. players) ^. hole) ++ _community (initTexas ps sls)
            putStrLn $ case compare 
-                             (fst $ getHighestHand $ ((head ((initTexas ps sls) ^. players)) ^. hole) ++ _community (initTexas ps sls))
-                             (fst $ getHighestHand $ ((last ((initTexas ps sls) ^. players)) ^. hole) ++ _community (initTexas ps sls))
+                             (fst $ getHighestHand $ (head (initTexas ps sls ^. players) ^. hole) ++ _community (initTexas ps sls))
+                             (fst $ getHighestHand $ (last (initTexas ps sls ^. players) ^. hole) ++ _community (initTexas ps sls))
                              of
                         EQ -> "Check!!"
-                        LT -> ((last ps) ^. name) ++ " Win!!"
-                        GT -> ((head ps) ^. name) ++ " Win!!"
-            where ps = ([initPlayer "Player1", initPlayer "Player2"])
-
-
-           -- Should be [4,32,624,3744,5108,10200,54912,123552,1098240,1302540]
-
--- main =  do mapM_ (print) $ map (\x -> (x, checkHands x)) $ comb 5 $ initCards
+                        LT -> (last ps ^. name) ++ " Win!!"
+                        GT -> (head ps ^. name) ++ " Win!!"
+            where ps = [initPlayer "Player1", initPlayer "Player2"]
